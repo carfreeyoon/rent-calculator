084714db9db4e2f3e302e4ebcd4bdf076ca23bd4
carfreeyoon <carfreeyoon@gmail.com>
2026-05-28 12:25:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -441,7 +441,7 @@ if not IS_CLIENT_VIEW:
 
         for idx, item in enumerate(st.session_state.quote_history):
 
-            history_col1, history_col2 = st.sidebar.columns([0.9, 1.1])
+            history_col1, history_col2 = st.sidebar.columns([0.9, 0.42])
 
             with history_col1:
                 if st.button(
